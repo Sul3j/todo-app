87b0bffc3172e54f4d193134643a31573e44584c
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-05-30 19:24:41 +0200

    add todo model and controller

---
+++ TodoAppAPI/TodoAppAPI/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoAppAPI.Models;
+
+namespace TodoAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Todo>>> GetTodos()
+        {
+
+        }
+    }
+}
